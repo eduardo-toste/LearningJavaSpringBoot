7b09577eed6d9ed6aee8ddafca049d6b4006c723
eduardo-toste <duciochetti@gmail.com>
2025-02-14 11:19:13 -0300

    buscando serie por ator utilizando derived queries

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -34,6 +34,7 @@ public class Principal {
                     2 - Buscar episódios
                     3 - Listar séries buscadas
                     4 - Buscar série por titulo
+                    5 - Buscar séries por ator
                     
                     0 - Sair                                 
                     """;
@@ -54,6 +55,9 @@ public class Principal {
                     break;
                 case 4:
                     buscarSeriePorTitulo();
+                    break;
+                case 5:
+                    buscarSeriesPorAtor();
                     break;
                 case 0:
                     System.out.println("Saindo...");
@@ -128,6 +132,17 @@ public class Principal {
         } else {
             System.out.println("Serie nao encontrada!");
         }
+    }
 
+    private void buscarSeriesPorAtor() {
+        System.out.println("Digite o nome do ator para busca: ");
+        String nomeAtor = leitura.nextLine();
+
+        System.out.println("Avaliacoes a partir de qual valor: ");
+        Double avaliacao = leitura.nextDouble();
+
+        List<Serie> seriesEncontradas = repositorio.findByAtoresContainingIgnoreCaseAndAvaliacaoGreaterThanEqual(nomeAtor, avaliacao);
+        System.out.println("Series em que " + nomeAtor + " trabalhou:");
+        seriesEncontradas.forEach(s -> System.out.println(s.getTitulo() + ", Avaliacao: " + s.getAvaliacao()));
     }
 }
